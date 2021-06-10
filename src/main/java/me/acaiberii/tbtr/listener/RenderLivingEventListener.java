1cfe67756be3012adfa99a43521d1c2c60e0f118
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-06-10 08:43:31 -0400

    Also remove this mixin, Forge already has an event.

---
+++ src/main/java/me/acaiberii/tbtr/listener/RenderLivingEventListener.java
@@ -0,0 +1,11 @@
+package me.acaiberii.tbtr.listener;
+
+import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class RenderLivingEventListener {
+    @SubscribeEvent
+    public void onLivingEvent(RenderLivingEvent event) {
+
+    }
+}
