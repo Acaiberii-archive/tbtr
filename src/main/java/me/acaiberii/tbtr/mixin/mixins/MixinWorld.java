aa1a7b39438c2e93b774706dd5f19ad576de2264
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-06-13 18:53:48 -0400

    also add this

---
+++ src/main/java/me/acaiberii/tbtr/mixin/mixins/MixinWorld.java
@@ -0,0 +1,16 @@
+package me.acaiberii.tbtr.mixin.mixins;
+
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(World.class)
+public class MixinWorld {
+    @Inject(method = "getRainStrength", at = @At("HEAD"), cancellable = true)
+    public void getRainStrength(float delta, CallbackInfoReturnable<Float> callback) {
+        callback.cancel();
+        callback.setReturnValue(0.0f);
+    }
+}
