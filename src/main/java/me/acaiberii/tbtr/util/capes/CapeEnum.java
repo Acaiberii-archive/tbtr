aecc7f79c29f3e5b643505396bb060998977b2cd
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-06-14 19:36:26 -0400

    Hopefully will work, optimize CapeUtil.java.

---
+++ src/main/java/me/acaiberii/tbtr/util/capes/CapeEnum.java
@@ -0,0 +1,8 @@
+package me.acaiberii.tbtr.util.capes;
+
+public enum CapeEnum {
+    BERII,
+    STELLA,
+    BROOK,
+    REGULAR
+}
--- src/main/java/me/acaiberii/tbtr/utility/CapeUtil.java
+++ src/main/java/me/acaiberii/tbtr/utility/CapeUtil.java
@@ -1,5 +1,7 @@
 package me.acaiberii.tbtr.utility;
 
+import me.acaiberii.tbtr.util.capes.CapeEnum;
+import me.acaiberii.tbtr.util.capes.CapeType;
 import org.json.simple.JSONObject;
 
 import java.io.BufferedReader;
@@ -9,6 +11,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -17,115 +20,39 @@ This is from Gav06's GavHack, taken from Gamesense. :)
 */
 
 public class CapeUtil {
-    private HashSet<UUID> regUuids = new HashSet<>();
-    private HashSet<UUID> brokUuids = new HashSet<>();
-    private HashSet<UUID> stellaUuids = new HashSet<>();
-    private HashSet<UUID> acaiUuids = new HashSet<>();
+    public HashSet<CapeType> capes = new HashSet<>();
 
     public CapeUtil() {
-        getRegCapes();
-        getBrokCapes();
-        getStellaCapes();
-        getAcaiCapes();
-    }
-
-    private void getRegCapes() {
         try {
-            URL regGithubFile = new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/reg-uuids");
-
-            BufferedReader regReader = null;
-            try {
-                regReader = new BufferedReader(new InputStreamReader(regGithubFile.openStream()));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String regLine;
-            while ((regLine = regReader.readLine()) != null) {
-                regUuids.add(UUID.fromString(regLine));
-            }
-        } catch (Exception e) {
+            CapeType berii = new CapeType(new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/berii-uuids"), CapeEnum.BERII);
+            capes.add(berii);
+            CapeType stella = new CapeType(new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/stella-uuids"), CapeEnum.STELLA);
+            capes.add(stella);
+            CapeType brok = new CapeType(new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/brok-uuids"), CapeEnum.BROOK);
+            capes.add(stella);
+            CapeType regular = new CapeType(new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/reg-uuids"), CapeEnum.REGULAR);
+            capes.add(regular);
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
 
-    private void getBrokCapes() {
-        try {
-            URL brokGithubFile = new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/brok-uuids");
-
-            BufferedReader brokReader = null;
-            try {
-                brokReader = new BufferedReader(new InputStreamReader(brokGithubFile.openStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String brokLine;
-            while ((brokLine = brokReader.readLine()) != null) {
-                brokUuids.add(UUID.fromString(brokLine));
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+    public boolean hasCape(CapeEnum capeEnum, UUID uuid) {
+        CapeType thisCape = getNullCape();
+        thisCape = getCape(capeEnum);
+        return thisCape.getCapes().contains(uuid);
     }
 
-    private void getStellaCapes() {
-        try {
-            URL stellaGithubFile = new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/stella-uuids");
-
-            BufferedReader stellaReader = null;
-            try {
-                stellaReader = new BufferedReader(new InputStreamReader(stellaGithubFile.openStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String stellaLine;
-            while ((stellaLine = stellaReader.readLine()) != null) {
-                stellaUuids.add(UUID.fromString(stellaLine));
+    public CapeType getCape(CapeEnum capeEnum) {
+        for (CapeType cape : capes) {
+            if (cape.getCapeEnum() == capeEnum) {
+                return cape;
             }
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        return getNullCape();
     }
 
-    private void getAcaiCapes() {
-        try {
-            URL acaiGithubFile = new URL("https://raw.githubusercontent.com/AcaiBerii/tbtr-capes/main/berii-uuids");
-
-            BufferedReader acaiReader = null;
-            try {
-                acaiReader = new BufferedReader(new InputStreamReader(acaiGithubFile.openStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            String acaiLine;
-            while ((acaiLine = acaiReader.readLine()) != null) {
-                acaiUuids.add(UUID.fromString(acaiLine));
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public boolean hasRegCape(UUID uuid) {
-        return regUuids.contains(uuid);
-    }
-
-    public boolean hasBrokCape(UUID uuid) {
-        return brokUuids.contains(uuid);
-    }
-
-    public boolean hasStellaCape(UUID uuid) {
-        return stellaUuids.contains(uuid);
-    }
-
-    public boolean hasAcaiCape(UUID uuid) {
-        return acaiUuids.contains(uuid);
+    public CapeType getNullCape() {
+        return new CapeType(null, null);
     }
 }
