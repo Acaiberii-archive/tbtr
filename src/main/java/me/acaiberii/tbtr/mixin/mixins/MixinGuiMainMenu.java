58e7da36e2dad6f875f3bd8723946f30d384a36e
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-06-14 12:12:46 -0400

    remove minecraft logo

--- src/main/java/me/acaiberii/tbtr/mixin/mixins/MixinGuiMainMenu.java
+++ src/main/java/me/acaiberii/tbtr/mixin/mixins/MixinGuiMainMenu.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiMainMenu.class)
@@ -14,4 +15,7 @@ public class MixinGuiMainMenu {
     private void drawScreenHook(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         FontRenderUtil.drawRainbowString("tbtr", 2, 2, 0.2, 10, 1f);
     }
+
+    @Redirect(method = "drawScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiMainMenu;drawTexturedModalRect(IIIIII)V"))
+    public void removeMenuLogoRendering(GuiMainMenu guiMainMenu, int x, int y, int textureX, int textureY, int width, int height) {}
 }
