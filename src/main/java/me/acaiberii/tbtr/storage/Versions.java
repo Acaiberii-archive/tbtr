f6982591666d9192c8eb806acb438f61822cafff
abcdefgberii <69873653+AcaiBerii@users.noreply.github.com>
2021-06-16 13:24:19 -0400

    bump

--- src/main/java/me/acaiberii/tbtr/storage/Versions.java
+++ src/main/java/me/acaiberii/tbtr/storage/Versions.java
@@ -1,6 +1,6 @@
 package me.acaiberii.tbtr.storage;
 
 public class Versions {
-    public static String version = "4.1.4";
+    public static String version = "4.1.5";
     public static String latestVersion = "";
 }
